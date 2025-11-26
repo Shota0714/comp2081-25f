02fa5cb8503d0c2e6824714a48d2e73bcc7d1093
Shota0714 <u.shota0714@gmail.com>
2025-11-25 20:46:27 -0500

    added some comments

--- lab-e/js/charts.js
+++ lab-e/js/charts.js
@@ -1,20 +1,25 @@
+// get height of rect
 let rect = document.querySelector("svg rect");
 rect = rect.getAttribute("height");
 rect = parseInt(rect, 10);
 console.log(rect);
 
+// capture value as an integer
 let text = document.querySelector("svg text").textContent;
 text = text.replaceAll("%", "");
 text = parseInt(text, 10);
 text = text / 100;
 console.log(text);
 
+// calculate height
 let wedge = text * rect;
 console.log(wedge);
 
+// grab the external css
 const cssRulesList = document.styleSheets[0].cssRules;
 console.log(cssRulesList)
 
+// loop through the css
 let svgActiveRule;
 for (let i = 0; i < cssRulesList.length; i ++) {
     if(cssRulesList[i].selectorText == "svg:active #wedge") {
@@ -22,8 +27,8 @@ for (let i = 0; i < cssRulesList.length; i ++) {
     }
 }
 
+// update the css declaration
 let propertyValue = wedge + "px";
 console.log(propertyValue);
-
 svgActiveRule.style.setProperty("height", propertyValue);
 
